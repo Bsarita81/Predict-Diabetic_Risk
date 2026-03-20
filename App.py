19bca7c75b9347aa3cbc9c4e0475a065506e6873
Bsarita81 <146192369+Bsarita81@users.noreply.github.com>
2026-03-20 11:53:56 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "26e6681e-2359-4bc8-9efd-db0374a252c0",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
